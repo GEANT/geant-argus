87a5e4a1dd0713897e5a84b60120a7770d564d87
Pelle Koster <pelle.koster@geant.org>
2024-05-07 10:14:50 +0200

    add prod logging config

--- src/geant_argus/settings/prod.py
+++ src/geant_argus/settings/prod.py
@@ -6,3 +6,16 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 COOKIE_DOMAIN = None
 CORS_ALLOW_ALL_ORIGINS = True
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
