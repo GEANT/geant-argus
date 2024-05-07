b728db0f20eae2fe21883910718e20a5cefc2a5f
Pelle Koster <pelle.koster@geant.org>
2024-05-07 07:50:02 +0200

    set allowed hosts

--- src/geant_argus/settings/prod.py
+++ src/geant_argus/settings/prod.py
@@ -1,5 +1,6 @@
-from .base import *
+from .base import *  # noqa: F403
 
-SECRET_KEY = get_str_env("ARGUS_SECRET_KEY")
+SECRET_KEY = get_str_env("ARGUS_SECRET_KEY")  # noqa: F405
 
 DEBUG = False
+ALLOWED_HOSTS = ["*"]
