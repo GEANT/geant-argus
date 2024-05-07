66d0b261709271a8a0b5ebfba985d2e8bdb9f55d
Pelle Koster <pelle.koster@geant.org>
2024-05-07 09:20:31 +0200

    take argus prod settings and tweak

--- src/geant_argus/settings/prod.py
+++ src/geant_argus/settings/prod.py
@@ -1,6 +1,7 @@
 from .base import *  # noqa: F403
+from argus.site.settings.prod import *  # noqa: F403
 
 SECRET_KEY = get_str_env("ARGUS_SECRET_KEY")  # noqa: F405
-
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
