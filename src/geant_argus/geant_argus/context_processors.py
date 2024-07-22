61d0837f5f5e0410e499280285140180840e0095
Pelle Koster <pelle.koster@geant.org>
2024-07-22 11:48:06 +0200

    some cleanup wrt themes

--- src/geant_argus/geant_argus/context_processors.py
+++ src/geant_argus/geant_argus/context_processors.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 
-def geant_global(request):
-    return {"theme": "geant_global"}
 
 def geant_theme(request):
     """Set the argus theme in the render context"""
