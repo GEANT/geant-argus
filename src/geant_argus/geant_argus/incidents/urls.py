345334e32be60bc2d7b69d5bd1763e0f1cc942b8
Pelle Koster <pelle.koster@geant.org>
2024-08-08 10:59:59 +0200

    make namespace more specific

--- src/geant_argus/geant_argus/incidents/urls.py
+++ src/geant_argus/geant_argus/incidents/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "geant"
+app_name = "geant-incidents"
 urlpatterns = [
     path("<int:pk>/ack", views.acknowledge_incident, name="ack-incident"),
 ]
