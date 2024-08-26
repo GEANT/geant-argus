4698cf78d113a0e69f068c0d30db06099695704b
PelleK <elfjes@users.noreply.github.com>
2024-08-26 16:08:39 +0200

    Color rows by severity (#20)

--- src/geant_argus/geant_argus/templatetags/incident_extras.py
+++ src/geant_argus/geant_argus/templatetags/incident_extras.py
@@ -1,5 +1,7 @@
 import json
+
 from argus.auth.models import User
+from argus.incident.models import Incident
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -44,3 +46,24 @@ def is_acked_by(incident, group: str) -> bool:
     """Backport of filter with the same name in argus-htmx-frontend"""
     # TODO: remove once argus-htmx-frontend v0.5 is released
     return incident.is_acked_by(group)
+
+
+@register.filter
+def row_classes(incident: Incident):
+    status = incident.metadata.get("status", "").upper()
+    if status == "CLEAR":
+        return "bg-incident-clear"
+    match IncidentSeverity(incident.level):
+        case IncidentSeverity.CRITICAL:
+            color = "bg-incident-critical"
+        case IncidentSeverity.MAJOR:
+            color = "bg-incident-major"
+        case IncidentSeverity.MINOR:
+            color = "bg-incident-minor"
+        case IncidentSeverity.WARNING:
+            color = "bg-incident-warning"
+        case _:
+            color = ""
+    if color and status == "CLOSED":
+        color = f"{color}/50"
+    return color
--- tailwindcss/tailwind.config.template.js
+++ tailwindcss/tailwind.config.template.js
@@ -1,64 +1,77 @@
+const baseArgusColors = {
+    'primary': '#006d91',
+    'primary-content': '#d1e1e9',
+    'secondary': '#f3b61f',
+    'secondary-content': '#140c00',
+    'accent': '#c84700',
+    'accent-content': '#f8dbd1',
+    'neutral': '#006d91',
+    'neutral-content': '#d1e1e9',
+    'base-100': '#edfaff',
+    'base-200': '#ced9de',
+    'base-300': '#b0babd',
+    'base-content': '#141516',
+    'info': '#0073e5',
+    'info-content': '#000512',
+    'success': '#008700',
+    'success-content': '#d3e7d1',
+    'warning': '#ee4900',
+    'warning-content': '#140200',
+    'error': '#e5545a',
+    'error-content': '#120203',
+}
+const incidentColors = {
+    '--color-incident-clear': '80, 200, 120',
+    '--color-incident-warning': '237, 250, 255', // same as base-100
+    '--color-incident-minor': '238, 194, 0',
+    '--color-incident-major': '238, 155, 0',
+    '--color-incident-critical': '240, 67, 67',
+}
+
 module.exports = {
     content: [
 {{ tailwind_content }}
+        'src/geant_argus/geant_argus/templatetags/**/*.py',
     ],
     theme: {
         borderWidth: {
             DEFAULT: '2px',
         },
-        extend: {},
+        extend: {
+            colors: {
+                'incident-major': 'rgba(var(--color-incident-major), <alpha-value>)',
+                'incident-minor': 'rgba(var(--color-incident-minor), <alpha-value>)',
+                'incident-critical': 'rgba(var(--color-incident-critical), <alpha-value>)',
+                'incident-warning': 'rgba(var(--color-incident-warning), <alpha-value>)',
+                'incident-clear': 'rgba(var(--color-incident-clear), <alpha-value>)',
+              },
+        },
     },
+    safelist: [
+        // these classes are dynamically generated so not seen by tailwind
+        {
+            pattern: /bg-incident-(clear|warning|minor|major|critical)\/50/,
+        },
+    ],
     daisyui: {
         themes: ['light', 'dark',
         {
             'argus': {
-                'primary': '#006d91',
-                'primary-content': '#d1e1e9',
-                'secondary': '#f3b61f',
-                'secondary-content': '#140c00',
-                'accent': '#c84700',
-                'accent-content': '#f8dbd1',
-                'neutral': '#006d91',
-                'neutral-content': '#d1e1e9',
-                'base-100': '#edfaff',
-                'base-200': '#ced9de',
-                'base-300': '#b0babd',
-                'base-content': '#141516',
-                'info': '#0073e5',
-                'info-content': '#000512',
-                'success': '#008700',
-                'success-content': '#d3e7d1',
-                'warning': '#ee4900',
-                'warning-content': '#140200',
-                'error': '#e5545a',
-                'error-content': '#120203',
+                ...baseArgusColors,
+                ...incidentColors
             },
         },
         {
             'geant': {
-                'primary': '#006d91',
-                'primary-content': '#d1e1e9',
-                'secondary': '#f3b61f',
-                'secondary-content': '#140c00',
-                'accent': '#006d91',
-                'accent-content': '#d1e1e9',
-                'neutral': '#006d91',
-                'neutral-content': '#d1e1e9',
-                'base-100': '#edfaff',
-                'base-200': '#ced9de',
-                'base-300': '#b0babd',
-                'base-content': '#141516',
-                'info': '#0073e5',
-                'info-content': '#000512',
-                'success': '#008700',
-                'success-content': '#d3e7d1',
+                ...baseArgusColors,
+                'accent': baseArgusColors['neutral'],
+                'accent-content': baseArgusColors['neutral-content'],
                 'warning': '#eec200',
-                'warning-content': '#140200',
-                'error': '#e5545a',
-                'error-content': '#120203',
+                ...incidentColors
             }
         }],
       },
+
     plugins: [
         require('@tailwindcss/aspect-ratio'),
         require('daisyui')
