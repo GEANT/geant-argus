becedcb5d0a1f2b92f8d0b6d6beb8bd7b972d883
Pelle Koster <pelle.koster@geant.org>
2024-08-26 16:35:16 +0200

    add admin link and redirect root to incidents listing

--- src/geant_argus/urls.py
+++ src/geant_argus/urls.py
@@ -1,9 +1,12 @@
-from argus.site.urls import urlpatterns
+from argus.site.urls import urlpatterns as argus_urlpatterns
 from django.urls import include, path
 from geant_argus.geant_argus.filters import urls as filter_urls
 from geant_argus.geant_argus.incidents import urls as incident_urls
+from django.views.generic.base import RedirectView
 
-urlpatterns += [
+urlpatterns = [
+    path("", RedirectView.as_view(url="incidents/", permanent=False)),
+    *argus_urlpatterns,
     path("geant/incidents/", include(incident_urls)),
     path("filters/", include(filter_urls)),
     path("", include("argus_htmx.urls")),
