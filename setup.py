3421f0fcc0b4787bbc65df24c645ed0ec0337572
Pelle Koster <pelle.koster@geant.org>
2024-08-27 16:31:34 +0200

    No longer support python versions without match statement

--- setup.py
+++ setup.py
@@ -36,8 +36,6 @@ setup(
     license_files=("LICENSE.txt",),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
