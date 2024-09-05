f0a9a47f5a94b45f0077e8755a3dc9326b666c3a
PelleK <elfjes@users.noreply.github.com>
2024-09-05 13:34:41 +0200

    Show status color in badge instead of whole row (#35)

--- src/geant_argus/geant_argus/templatetags/incident_extras.py
+++ src/geant_argus/geant_argus/templatetags/incident_extras.py
@@ -10,13 +10,54 @@ from ..incidents.severity import IncidentSeverity
 register = template.Library()
 
 
-@register.filter(name="to_severity")
-def level_to_severity(value):
-    """Removes all values of arg from the given string"""
+def _level_to_severity(value):
     min_level = min(IncidentSeverity)
     max_level = max(IncidentSeverity)
     level = max(min_level, min(max_level, value))
-    return IncidentSeverity(level).name
+    return IncidentSeverity(level)
+
+
+@register.filter(name="to_severity")
+def level_to_severity(value):
+    """Removes all values of arg from the given string"""
+    return _level_to_severity(value).name
+
+
+@register.filter(name="levelbadge")
+def level_to_badge(value):
+    severity = _level_to_severity(value)
+    match severity:
+        case IncidentSeverity.CRITICAL:
+            return "bg-incident-critical border-incident-critical"
+        case IncidentSeverity.MAJOR:
+            return "bg-incident-major border-incident-major"
+        case IncidentSeverity.MINOR:
+            return "bg-incident-minor border-incident-minor"
+        case _:
+            return "badge-outline-ghost"
+
+
+def _incident_status(incident: Incident):
+    if incident.open:
+        return upperfirst(incident.metadata.get("status", "Active"))
+    return "Closed"
+
+
+@register.filter(name="incidentstatus")
+def incident_status_text(incident: Incident):
+    return _incident_status(incident)
+
+
+@register.filter(name="statusbadge")
+def incident_status_badge(incident: Incident):
+    status = _incident_status(incident)
+    match status:
+        case "Active":
+            return "badge-neutral"
+        case "Clear":
+            return "bg-incident-clear border-incident-clear"
+        case "Closed":
+            return "badge-outline-ghost"
 
 
 @register.filter
