d518eeec8897e8b7ff15a4cb43da2203d220a249
Pelle Koster <pelle.koster@geant.org>
2024-09-06 11:56:27 +0200

    fix status update url

--- src/geant_argus/geant_argus/status/urls.py
+++ src/geant_argus/geant_argus/status/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "geant-status"
 urlpatterns = [
     path("", views.service_status, name="status"),
-    path("/update", views.update_inventory, name="update"),
+    path("update/", views.update_inventory, name="update"),
 ]
