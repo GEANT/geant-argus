0f38ff3e7c1f33740ce7aa2eeec7dfcd8dbbd6c3
Pelle Koster <pelle.koster@geant.org>
2024-09-20 11:35:26 +0200

    Specialize endpoint table columns based on endpoint type using metadata v0a5

--- src/geant_argus/geant_argus/context_processors.py
+++ src/geant_argus/geant_argus/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 
 def geant_theme(request):
-    """Set the argus theme in the render context"""
+    """Additional context variables specific to Geant"""
     return {
         "theme": request.session.get("theme", getattr(settings, "DEFAULT_THEME", "geant")),
         "path_to_stylesheet": getattr(settings, "DEFAULT_TW_CSS", "geant.css"),
@@ -15,4 +15,74 @@ def geant_theme(request):
             if getattr(settings, "STATUS_CHECKER_ENABLED")
             else None
         ),
+        "incident_details_tables": {
+            "bgp": [
+                {
+                    "name": "Hostname",
+                    "template": "htmx/incident_details/bgp_hostname.html",
+                    "is_endpoint_column": True,
+                },
+                {
+                    "name": "Remote Peer",
+                    "template": "htmx/incident_details/bgp_remote_peer.html",
+                    "is_endpoint_column": True,
+                },
+                {"name": "Alarm ID", "template": "htmx/incident_details/event_id.html"},
+                {"name": "Status", "template": "htmx/incident_details/event_status.html"},
+                {"name": "Init Time", "template": "htmx/incident_details/event_init_time.html"},
+                {"name": "Clear Time", "template": "htmx/incident_details/event_clear_time.html"},
+                {"name": "Properties", "template": "htmx/incident_details/event_properties.html"},
+            ],
+            "link": [
+                {
+                    "name": "Hostname",
+                    "template": "htmx/incident_details/bgp_hostname.html",
+                    "is_endpoint_column": True,
+                },
+                {
+                    "name": "Interface",
+                    "template": "htmx/incident_details/bgp_interface.html",
+                    "is_endpoint_column": True,
+                },
+                {"name": "Alarm ID", "template": "htmx/incident_details/event_id.html"},
+                {"name": "Status", "template": "htmx/incident_details/event_status.html"},
+                {"name": "Init Time", "template": "htmx/incident_details/event_init_time.html"},
+                {"name": "Clear Time", "template": "htmx/incident_details/event_clear_time.html"},
+                {"name": "Properties", "template": "htmx/incident_details/event_properties.html"},
+            ],
+            "coriant": [
+                {
+                    "name": "NE Name",
+                    "template": "htmx/incident_details/optical_ne_name.html",
+                    "is_endpoint_column": True,
+                },
+                {
+                    "name": "Port",
+                    "template": "htmx/incident_details/optical_port.html",
+                    "is_endpoint_column": True,
+                },
+                {"name": "Alarm ID", "template": "htmx/incident_details/event_id.html"},
+                {"name": "Status", "template": "htmx/incident_details/event_status.html"},
+                {"name": "Init Time", "template": "htmx/incident_details/event_init_time.html"},
+                {"name": "Clear Time", "template": "htmx/incident_details/event_clear_time.html"},
+                {"name": "Properties", "template": "htmx/incident_details/event_properties.html"},
+            ],
+            "infinera": [
+                {
+                    "name": "NE Name",
+                    "template": "htmx/incident_details/optical_ne_name.html",
+                    "is_endpoint_column": True,
+                },
+                {
+                    "name": "Port",
+                    "template": "htmx/incident_details/optical_port.html",
+                    "is_endpoint_column": True,
+                },
+                {"name": "Alarm ID", "template": "htmx/incident_details/event_id.html"},
+                {"name": "Status", "template": "htmx/incident_details/event_status.html"},
+                {"name": "Init Time", "template": "htmx/incident_details/event_init_time.html"},
+                {"name": "Clear Time", "template": "htmx/incident_details/event_clear_time.html"},
+                {"name": "Properties", "template": "htmx/incident_details/event_properties.html"},
+            ],
+        },
     }
