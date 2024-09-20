e0d7f1c9cf8b79775f736333bc58fbb2e5b06b2d
PelleK <elfjes@users.noreply.github.com>
2024-09-20 17:08:05 +0200

    Add duration to incident listing and improve multiline fields (#52)

---
+++ src/geant_argus/geant_argus/templatetags/template_utils.py
@@ -0,0 +1,33 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
+@register.filter
+def smooth_timedelta(timedeltaobj):
+    """Convert a datetime.timedelta object into Days, Hours, Minutes, Seconds."""
+    secs = timedeltaobj.total_seconds()
+    timetot = ""
+    if secs > 86400:  # 60sec * 60min * 24hrs
+        days = secs // 86400
+        timetot += "{}d".format(int(days))
+        secs = secs - days * 86400
+
+    if secs > 3600:
+        hrs = secs // 3600
+        timetot += " {}h".format(int(hrs))
+        secs = secs - hrs * 3600
+
+    if secs > 60:
+        mins = secs // 60
+        timetot += " {}m".format(int(mins))
+        secs = secs - mins * 60
+
+    if secs > 0:
+        timetot += " {}s".format(int(secs))
+    return timetot
