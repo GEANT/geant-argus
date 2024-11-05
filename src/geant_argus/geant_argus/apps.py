d8c3d478d00d4fb430ddf6764f0ade7fbfe7e857
Pelle Koster <pelle.koster@geant.org>
2024-11-05 09:24:41 +0100

    WIP bulk actions

--- src/geant_argus/geant_argus/apps.py
+++ src/geant_argus/geant_argus/apps.py
@@ -2,9 +2,18 @@ import pathlib
 from django.apps import AppConfig
 
 
+
 class GeantConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "geant_argus.geant_argus"
 
     def tailwind_css_files(self):
         yield from pathlib.Path(__file__).parent.glob("tailwindcss/*.css")
+
+    def ready(self) -> None:
+        from argus_htmx.incidents.views import INCIDENT_UPDATE_ACTIONS, DescriptionOptionalForm
+        from .incidents.bulk_actions import bulk_close_incidents
+
+        del INCIDENT_UPDATE_ACTIONS['reopen']
+        del INCIDENT_UPDATE_ACTIONS['update-ticket']
+        INCIDENT_UPDATE_ACTIONS["close"] = (DescriptionOptionalForm, bulk_close_incidents)
--- src/geant_argus/geant_argus/dashboard_alarms.py
+++ src/geant_argus/geant_argus/dashboard_alarms.py
@@ -6,11 +6,23 @@ CLOSE_ALARM_URL = "/alarms/{}/close"
 CLEAR_ALARM_URL = "/alarms/{}/clear"
 
 
-def update_alarm(alarm_id, payload):
-    API_URL = getattr(settings, "DASHBOARD_ALARMS_API_URL", None)
-    if not API_URL:
+def api_url():
+    result = getattr(settings, "DASHBOARD_ALARMS_API_URL", None)
+    if not result:
         raise ValueError("Please set the ARGUS_DASHBOARD_ALARMS_API_URL environment setting")
-    return _succeed_request("PATCH", API_URL + UPDATE_ALARM_URL.format(alarm_id), json=payload)
+    return result
+
+
+def update_alarm(alarm_id, payload):
+    return _succeed_request("PATCH", api_url() + UPDATE_ALARM_URL.format(alarm_id), json=payload)
+
+
+def close_alarm(alarm_id):
+    return _succeed_request("POST", api_url() + CLOSE_ALARM_URL.format(alarm_id))
+
+
+def clear_alarm(alarm_id, payload):
+    return _succeed_request("POST", api_url() + CLEAR_ALARM_URL.format(alarm_id), json=payload)
 
 
 def _succeed_request(*args, timeout=5, **kwargs) -> bool:
