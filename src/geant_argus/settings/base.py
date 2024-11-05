b5836ca863113a1bec66efd784fa878103eedd4c
Pelle Koster <pelle.koster@geant.org>
2024-11-05 10:04:10 +0100

    reorder settings

--- src/geant_argus/settings/base.py
+++ src/geant_argus/settings/base.py
@@ -21,10 +21,6 @@ MIDDLEWARE += [  # noqa: F405
 if "DATABASES" in globals():
     DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa: F405
 
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
-
-
 MEDIA_PLUGINS = [
     "argus.notificationprofile.media.email.EmailNotification",
 ]
@@ -34,12 +30,36 @@ INDELIBLE_INCIDENTS = False
 # geant_argus/argus_site/apps.py
 TEMPLATES[0]["DIRS"] = []  # noqa: F405
 
+
 #  Authentication
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
+    "social_core.backends.open_id_connect.OpenIdConnectAuth",
 ]
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "geant_argus.auth.get_userdata",
+    # Here we deviate from the default pipeline to support whitelisting users
+    "geant_argus.auth.require_existing_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+)
+
+SOCIAL_AUTH_LOGIN_ERROR_URL = "/accounts/login"
+
+ARGUS_OIDC_METHOD_NAME = "Geant AAI"
+SOCIAL_AUTH_OIDC_OIDC_ENDPOINT = get_str_env("ARGUS_OIDC_URL")
+SOCIAL_AUTH_OIDC_KEY = get_str_env("ARGUS_OIDC_CLIENT_ID")
+SOCIAL_AUTH_OIDC_SECRET = get_str_env("ARGUS_OIDC_SECRET")
+
 
 # Theming
 DEFAULT_THEME = "geant"
@@ -139,29 +159,3 @@ MUST_ACK_WITHIN_MINUTES = get_int_env("ARGUS_MUST_ACK_WITHIN_MINUTES", default=N
 
 # Dashboard Alarms API
 DASHBOARD_ALARMS_API_URL = os.getenv("ARGUS_DASHBOARD_ALARMS_API_URL")
-
-AUTHENTICATION_BACKENDS = [
-    "django.contrib.auth.backends.ModelBackend",
-    "social_core.backends.open_id_connect.OpenIdConnectAuth",
-]
-
-SOCIAL_AUTH_JSONFIELD_ENABLED = True
-SOCIAL_AUTH_PIPELINE = (
-    "social_core.pipeline.social_auth.social_details",
-    "social_core.pipeline.social_auth.social_uid",
-    "social_core.pipeline.social_auth.auth_allowed",
-    "social_core.pipeline.social_auth.social_user",
-    "social_core.pipeline.social_auth.associate_user",
-    "geant_argus.auth.get_userdata",
-    # Here we deviate from the default pipeline to support whitelisting users
-    "geant_argus.auth.require_existing_user",
-    "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
-)
-
-SOCIAL_AUTH_LOGIN_ERROR_URL = "/accounts/login"
-
-ARGUS_OIDC_METHOD_NAME = "Geant AAI"
-SOCIAL_AUTH_OIDC_OIDC_ENDPOINT = get_str_env("ARGUS_OIDC_URL")
-SOCIAL_AUTH_OIDC_KEY = get_str_env("ARGUS_OIDC_CLIENT_ID")
-SOCIAL_AUTH_OIDC_SECRET = get_str_env("ARGUS_OIDC_SECRET")
