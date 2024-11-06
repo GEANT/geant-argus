77244e62c7d11456e29538d0d52a2bbde469a826
Pelle Koster <pelle.koster@geant.org>
2024-11-06 14:56:28 +0100

    add clear modal

--- src/geant_argus/geant_argus/apps.py
+++ src/geant_argus/geant_argus/apps.py
@@ -2,7 +2,6 @@ import pathlib
 from django.apps import AppConfig
 
 
-
 class GeantConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "geant_argus.geant_argus"
@@ -12,8 +11,13 @@ class GeantConfig(AppConfig):
 
     def ready(self) -> None:
         from argus_htmx.incidents.views import INCIDENT_UPDATE_ACTIONS, DescriptionOptionalForm
-        from .incidents.bulk_actions import bulk_close_incidents
+        from .incidents.bulk_actions import (
+            bulk_close_incidents,
+            bulk_clear_incidents,
+            ClearAlarmForm,
+        )
 
-        del INCIDENT_UPDATE_ACTIONS['reopen']
-        del INCIDENT_UPDATE_ACTIONS['update-ticket']
+        del INCIDENT_UPDATE_ACTIONS["reopen"]
+        del INCIDENT_UPDATE_ACTIONS["update-ticket"]
         INCIDENT_UPDATE_ACTIONS["close"] = (DescriptionOptionalForm, bulk_close_incidents)
+        INCIDENT_UPDATE_ACTIONS["clear"] = (ClearAlarmForm, bulk_clear_incidents)
--- src/geant_argus/geant_argus/incidents/bulk_actions.py
+++ src/geant_argus/geant_argus/incidents/bulk_actions.py
@@ -2,11 +2,12 @@ from typing import Any, Dict
 from argus_htmx.utils import bulk_close_queryset
 from django import forms
 from django.http import HttpResponseServerError
-from geant_argus.geant_argus.dashboard_alarms import close_alarm
+from geant_argus.geant_argus.dashboard_alarms import close_alarm, clear_alarm
+from django.utils import timezone
 
 
 class ClearAlarmForm(forms.Form):
-    clear_time = forms.DateTimeField(required=False)
+    timestamp = forms.DateTimeField(required=False)
 
 
 def bulk_close_incidents(actor, qs, data: Dict[str, Any]):
@@ -15,16 +16,20 @@ def bulk_close_incidents(actor, qs, data: Dict[str, Any]):
         if not close_alarm(incident.source_incident_id):
             raise HttpResponseServerError("Error while closing incident")
         incident.metadata["status"] = "CLOSED"
+        incident.metadata["clear_time"] = data["timestamp"].isoformat()
         incident.save()
 
     return incidents
 
 
 def bulk_clear_incidents(actor, qs, data: Dict[str, Any]):
-    for incident in qs:
-        if not close_alarm(incident.source_incident_id):
-            raise HttpResponseServerError("Error while closing incident")
-        incident.metadata["status"] = "CLOSED"
+    clear_time = (data["timestamp"] or timezone.now()).isoformat()
+    incidents = list(qs)
+    for incident in incidents:
+        if not clear_alarm(incident.source_incident_id, {"clear_time": clear_time}):
+            raise HttpResponseServerError("Error while clearing incident")
+        incident.metadata["status"] = "CLEAR"
+        incident.metadata["clear_time"] = clear_time
         incident.save()
 
     return incidents
