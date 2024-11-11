86f1727a7d8515260b052a20dd0f9f4105425e18
Pelle Koster <pelle.koster@geant.org>
2024-11-11 16:29:06 +0100

    fix typo

--- src/geant_argus/geant_argus/incidents/urls.py
+++ src/geant_argus/geant_argus/incidents/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "geant-incidents"
 urlpatterns = [
     path("<int:pk>/ack", views.acknowledge_incident, name="ack-incident"),
-    path("<int:pk>/updaate", views.update_incident, name="update-incident"),
+    path("<int:pk>/update", views.update_incident, name="update-incident"),
 ]
