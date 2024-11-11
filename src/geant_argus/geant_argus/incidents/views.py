8cda638f848f4fe96ec020d0c9a082a161aad4a9
Pelle Koster <pelle.koster@geant.org>
2024-11-11 16:27:51 +0100

    only update alarm when there's something to update

--- src/geant_argus/geant_argus/incidents/views.py
+++ src/geant_argus/geant_argus/incidents/views.py
@@ -1,10 +1,11 @@
 from argus.incident.models import Incident
 from django import forms
 from django.conf import settings
-from django.http import HttpResponseServerError, HttpResponseBadRequest
+from django.contrib import messages
+from django.http import HttpResponseBadRequest, HttpResponseServerError
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
-from django.contrib import messages
+
 from geant_argus.geant_argus.dashboard_alarms import update_alarm
 from geant_argus.geant_argus.view_helpers import HtmxHttpRequest, refresh
 
@@ -31,6 +32,8 @@ def update_incident(request: HtmxHttpRequest, pk: int):
 
     incident = get_object_or_404(Incident, id=pk)
     payload = {k: v for k, v in form.cleaned_data.items() if v is not None}
+    if not payload:
+        return refresh(request, "htmx:incident-list")
 
     if not update_alarm(incident.source_incident_id, payload=payload):
         messages.error(request, f"Error while updating alarm {incident.source_incident_id}")
