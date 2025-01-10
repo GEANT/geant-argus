894ed9256e82ffa84e2bf3fb9ce3767127653756
Pelle Koster <pelle.koster@geant.org>
2025-01-10 10:15:57 +0100

    follow Argus template naming convention

--- src/geant_argus/geant_argus/user/views.py
+++ src/geant_argus/geant_argus/user/views.py
@@ -1,5 +1,5 @@
-from argus.auth.utils import save_preference
-from argus.htmx.incidents.views import HtmxHttpRequest
+# from argus.auth.utils import save_preference
+from argus.htmx.incident.views import HtmxHttpRequest
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django_htmx.http import HttpResponseClientRefresh
@@ -7,6 +7,6 @@ from django_htmx.http import HttpResponseClientRefresh
 
 @require_POST
 def change_aural_alert(request: HtmxHttpRequest) -> HttpResponse:
-    save_preference(request, request.POST, "geant_argus", "aural_alert")
+    # save_preference(request, request.POST, "geant_argus", "aural_alert")
 
     return HttpResponseClientRefresh()
