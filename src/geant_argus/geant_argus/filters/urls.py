af53800feada37c2dc81ecaef747a17f0daff028
Pelle Koster <pelle.koster@geant.org>
2025-01-16 16:00:10 +0100

    support storing filters in session for temporary usage

--- src/geant_argus/geant_argus/filters/urls.py
+++ src/geant_argus/geant_argus/filters/urls.py
@@ -6,8 +6,10 @@ from . import views
 app_name = "geant-filters"
 urlpatterns = [
     path("", views.list_filters, name="filter-list"),
-    path("new/", views.edit_filter, name="edit-filter"),
+    path("run/", views.store_temporary_filter, name="run-filter"),
+    path("clear/", views.clear_temporary_filter, name="clear-filter"),
     path("new/save/", views.save_filter, name="save-new-filter"),
+    path("new/", views.edit_filter, name="edit-filter"),
     path("<int:pk>/", views.edit_filter, name="edit-filter"),
     path("<int:pk>/save/", views.save_filter, name="save-filter"),
     path("filter-text/", views.get_filter_text, name="filter-text"),
