43e201f76334c326b20689674df7f17e65aabdfb
geant-release-service <swd@geant.org>
2025-03-31 15:02:13 +0000

    incremented version for next development iteration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geant_argus",
-    version="0.27",
+    version="0.28",
     author="GEANT",
     author_email="swd@geant.org",
     description="Dashboard V3 framework",
