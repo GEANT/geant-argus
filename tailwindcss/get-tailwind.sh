36a0835d6cd8e87564547a8be8a01678c92e1425
Pelle Koster <pelle.koster@geant.org>
2025-04-03 12:01:21 +0200

    simplify get-tailwind script

--- tailwindcss/get-tailwind.sh
+++ tailwindcss/get-tailwind.sh
@@ -2,23 +2,19 @@
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null
 
-TAILWIND_EXTRA_VERSION=$(cat VERSION)
+TAILWIND_EXTRA_VERSION=v1.7.27
 TAILWIND_EXTRA_BASE_URL=https://github.com/dobicinaitis/tailwind-cli-extra/releases/download/${TAILWIND_EXTRA_VERSION}/tailwindcss-extra
 
-arch=$(uname -a | tr '[:upper:]' '[:lower:]')
-
-if [[ $arch == *"linux"* && $arch == *"x86_64"* ]]; then
-    ARCH_SPECIFIER="linux-x64"
-elif [[ $arch == *"linux"* && $arch == *"arm64"* ]]; then
-    ARCH_SPECIFIER="linux-arm64"
-elif [[ $arch == *"darwin"* && $arch == *"x86_64"* ]]; then
-    ARCH_SPECIFIER="macos-x64"
-elif [[ $arch == *"darwin"* && $arch == *"arm64"* ]]; then
-    ARCH_SPECIFIER="macos-arm64"
-else
-    echo "Unsupported platform '$(uname -a)'"
-    exit 1
-fi
+case $(uname -a | tr '[:upper:]' '[:lower:]') in
+    *linux*x86_64*) ARCH_SPECIFIER="linux-x64" ;;
+    *linux*arm64*) ARCH_SPECIFIER="linux-arm64" ;;
+    *darwin*x86_64*) ARCH_SPECIFIER="macos-x64" ;;
+    *darwin*arm64*) ARCH_SPECIFIER="macos-arm64" ;;
+    *)
+        echo "Unsupported platform '$(uname -a)'"
+        exit 1
+        ;;
+esac
 
 echo "Downloading tailwindcss extra ${TAILWIND_EXTRA_VERSION} for ${ARCH_SPECIFIER}"
 curl -sLo tailwindcss ${TAILWIND_EXTRA_BASE_URL}-${ARCH_SPECIFIER}
