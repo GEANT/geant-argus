cce6419484fc6a98544dc739c0ffdd1db6c89c54
Maryam Mokhtarifar <maryam.mokhtarifar@geant.org>
2025-06-18 16:03:45 +0200

    Refactor theme handling in CSS and HTML

--- src/geant_argus/geant_argus/static/js/theme-handler.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener('htmx:afterSwap', () => {
-  const selected = document.querySelector('input[name="theme"]:checked');
-  if (selected?.value) {
-    document.documentElement.setAttribute('data-theme', selected.value);
-  }
-});
