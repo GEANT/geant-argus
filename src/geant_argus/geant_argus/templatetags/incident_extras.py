c37b69f7bd59ac614d69302bea8860af2e6256dd
James Ladd <james.ladd@geant.org>
2025-08-26 10:50:07 +0100

    Fix stuck badge color

--- src/geant_argus/geant_argus/templatetags/incident_extras.py
+++ src/geant_argus/geant_argus/templatetags/incident_extras.py
@@ -91,7 +91,7 @@ def incident_status_badge(incident: Incident):
         case "Clear":
             return "incident-clear"
         case "Stuck":
-            return "incident-high"
+            return "incident-medium"
         case "Closed":
             return "incident-default"
 
