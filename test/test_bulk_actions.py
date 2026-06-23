0b74302bbb6b17d6b486db4af7ce563a7cf4ffaf
James Ladd <james.ladd@geant.org>
2026-06-23 15:25:13 +0100

    Send actor username to dashboard web API when closing or clearing

--- test/test_bulk_actions.py
+++ test/test_bulk_actions.py
@@ -255,6 +255,8 @@ def test_bulk_clear_incidents(mock_clear_alarm, default_user):
     assert len(incidents) == 1
     assert incidents[0].metadata["status"] == "CLEAR"
     assert incidents[0].metadata["clear_time"] == clear_time
-    assert mock_clear_alarm.call_args == ((qs[0].source_incident_id, {"clear_time": clear_time}),)
+    assert mock_clear_alarm.call_args == (
+        (qs[0].source_incident_id, {"clear_time": clear_time, "username": actor.username}),
+    )
     assert mock_clear_alarm.call_count == 1
     assert qs[0].save.call_count == 1
